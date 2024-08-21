adc6b1c69569fdd3d6028ed1bdcda6e13502b64d
yapetrenka <yapetrenka@yandex.ru>
2024-08-21 18:57:28 +0300

    3-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  const arr = str
+    .split(' ')
+    .filter(item => isFinite(item))
+    .sort( (a, b) => a - b );
+
+  result.min = +arr.shift();
+  result.max = +arr.pop();
+
+  return result;
 }

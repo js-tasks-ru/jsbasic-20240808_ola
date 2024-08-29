f874ae835e4ddead0c8b3bcf1cb55a4dc1583d72
yapetrenka <yapetrenka@yandex.ru>
2024-08-29 11:14:36 +0300

    remove event

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,7 +1,7 @@
 function toggleText() {
   const button = document.querySelector('.toggle-text-button');
   const text = document.querySelector('#text');
-  button.addEventListener('click', (event) => {
+  button.addEventListener('click', () => {
     text.hidden = !text.hidden;
   });
 }
